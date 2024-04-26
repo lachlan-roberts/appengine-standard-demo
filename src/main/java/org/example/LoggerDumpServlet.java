66f01060c00fdfa06f7e4fa09a4546e1b2c1fb68
Lachlan Roberts <lachlan@webtide.com>
2024-04-26 10:28:54 +1000

    updates
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- src/main/java/org/example/LoggerDumpServlet.java
+++ src/main/java/org/example/LoggerDumpServlet.java
@@ -7,13 +7,11 @@ import java.util.Arrays;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LoggerDumpServlet extends HttpServlet
 {
     private static final java.util.logging.Logger JUL_LOGGER = java.util.logging.Logger.getLogger(LoggerDumpServlet.class.getName());
-    private static final Logger SLF4J_LOGGER = LoggerFactory.getLogger(LoggerDumpServlet.class.getName());
+//    private static final Logger SLF4J_LOGGER = LoggerFactory.getLogger(LoggerDumpServlet.class.getName());
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException
@@ -25,12 +23,13 @@ public class LoggerDumpServlet extends HttpServlet
         System.err.println(getClass().getSimpleName() + " logging at System.err.");
         System.out.println(getClass().getSimpleName() + " logging at System.out.");
 
-        SLF4J_LOGGER.trace(getClass().getSimpleName() + " logging SLF4J at trace.");
-        SLF4J_LOGGER.debug(getClass().getSimpleName() + " logging SLF4J at debug.");
-        SLF4J_LOGGER.info(getClass().getSimpleName() + " logging SLF4J at info.");
-        SLF4J_LOGGER.warn(getClass().getSimpleName() + " logging SLF4J at warn.");
-        SLF4J_LOGGER.error(getClass().getSimpleName() + " logging SLF4J at error.");
+//        SLF4J_LOGGER.trace(getClass().getSimpleName() + " logging SLF4J at trace.");
+//        SLF4J_LOGGER.debug(getClass().getSimpleName() + " logging SLF4J at debug.");
+//        SLF4J_LOGGER.info(getClass().getSimpleName() + " logging SLF4J at info.");
+//        SLF4J_LOGGER.warn(getClass().getSimpleName() + " logging SLF4J at warn.");
+//        SLF4J_LOGGER.error(getClass().getSimpleName() + " logging SLF4J at error.");
 
+        System.err.println("loggerLevel: " + JUL_LOGGER.getLevel());
         JUL_LOGGER.finest(getClass().getSimpleName() + " logging JUL at finest.");
         JUL_LOGGER.finer(getClass().getSimpleName() + " logging JUL at finer.");
         JUL_LOGGER.fine(getClass().getSimpleName() + " logging JUL at fine.");
