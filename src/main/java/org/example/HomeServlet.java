eabb3a23a24267b09068d37d1ee54495b1a17238
Lachlan Roberts <lachlan@webtide.com>
2024-04-30 14:30:13 +1000

    additional testing of defaultServlet
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

---
+++ src/main/java/org/example/HomeServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.getWriter().println("home servlet");
+    }
+}
